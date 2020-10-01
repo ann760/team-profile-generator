a104cad62186ab23fcbc5906d4d8d73edd18ea77
Ann-Marie <anniedillier@gmail.com>
2020-10-01 00:05:35 -0700

    resolove copyFile

--- Index.js
+++ Index.js
@@ -299,12 +299,15 @@ const writeFile = (fileContent) => {
   });
 };
 
-fs.copyFile("./src/style.css", "./dist/style.css", (err) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-});
+const copyFile = () => {
+  //return new Promise((resolve, reject) => {
+  fs.copyFile("./src/style.css", "./dist/style.css", (err) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+  })
+};
 
 promptManager()
   .then(promptEmployee)
